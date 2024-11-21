1dbff2a8365097772e290543fe58621805780f69
pravinsam <pravinsamr@gmail.com>
2024-11-21 06:35:38 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// let active  =  document.getElementById('webdevelopment')
+//
+// active.addEventListener('click', () => {
+//     active.classList.toggle('active')
+// })
+//
+// let active1  =  document.getElementById('Database')
+//
+// active1.addEventListener('click', () => {
+//     active1.classList.toggle('active')
+// })
+
+
+const webdt = document.getElementById('webdt');
+const datab = document.getElementById('datab');
+
+function webdt() {
+    webd.addEventListener('click', (e) => {
+        e.classList.toggle('active');
+    })
+}
+
